21783c3499bb88432d0ec8bd052c369f26f2da75
Carlos Diaz <carl894@hotmail.com>
2021-11-14 14:32:48 -0500

    Delete proyectoC4G6 directory

--- proyectoC4G6/back/models/usuarios.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const usuarioSchema = new Schema({
-    usuario:    {type: String, required: true,  max:100},
-    pass:   {type: String, required: true,  max:128} //SIEMPRE debe ser 128 por la encriptacion.
-});
-
-module.exports = mongoose.model("usuarios", usuarioSchema);
